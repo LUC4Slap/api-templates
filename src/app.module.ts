3acf9c60218bbbb15f982c8d693744962470eebf
Lucas Almeida Petrolli <lucaslap27@gmail.com>
2025-07-03 12:12:30 -0400

    ✨ Resource
    Cria resource de Template e adiciona guard para acessar rotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TemplateModule } from './template/template.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TemplateModule],
   controllers: [],
   providers: [],
 })
---
+++ src/template/template.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { TemplateService } from './template.service';
+import { CreateTemplateDto } from './dto/create-template.dto';
+import { UpdateTemplateDto } from './dto/update-template.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+
+@Controller('template')
+export class TemplateController {
+  constructor(private readonly templateService: TemplateService) {}
+
+  @UseGuards(AuthGuard)
+  @Post()
+  create(@Body() createTemplateDto: CreateTemplateDto) {
+    return this.templateService.create(createTemplateDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll() {
+    return this.templateService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.templateService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTemplateDto: UpdateTemplateDto,
+  ) {
+    return this.templateService.update(+id, updateTemplateDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.templateService.remove(+id);
+  }
+}
